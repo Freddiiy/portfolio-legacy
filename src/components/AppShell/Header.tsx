f5f9dbdc5016d8d3db3e63138e6d603e6b4d4ebd
Freddiiy <frederikgaller@live.dk>
2022-06-29 20:43:51 +0200

    init

--- src/components/AppShell/Header.tsx
+++ src/components/AppShell/Header.tsx
@@ -1,10 +1,14 @@
 import {ReactNode} from "react";
+import Image from "next/image";
 
 export default function Header({children}: {children: ReactNode}) {
 	return (
 		<>
-			<div className={"fixed transition-opacity z-40 w-full h-16 border-b bg-white border-2 backdrop-blur"}>
-				<div className={""}>
+			<div className={"fixed transition-opacity z-40 w-full h-16 border-b bg-transparent backdrop-blur"}>
+				<div className={"container items-center justify-between"}>
+					<div className={"rounded-full"}>
+						<Image src={"/profile.png"} alt={""} layout={"fill"}/>
+					</div>
 
 				</div>
 			</div>
