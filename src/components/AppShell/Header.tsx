d0d711a119c3be4b95ea78f574fc94bf813f26dd
Freddiiy <frederikgaller@live.dk>
2022-06-29 20:29:07 +0200

    init

--- src/components/AppShell/Header.tsx
+++ src/components/AppShell/Header.tsx
@@ -4,7 +4,9 @@ export default function Header({children}: {children: ReactNode}) {
 	return (
 		<>
 			<div className={"fixed transition-opacity z-40 w-full h-16 border-b bg-white border-2 backdrop-blur"}>
+				<div className={""}>
 
+				</div>
 			</div>
 			<main className={"pt-16"}>
 				{children}
