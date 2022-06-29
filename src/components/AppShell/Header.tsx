ba023d5ddd724b0d8d20c0c9d2c257798a051479
Freddiiy <frederikgaller@live.dk>
2022-06-29 21:02:54 +0200

    start of header

--- src/components/AppShell/Header.tsx
+++ src/components/AppShell/Header.tsx
@@ -1,14 +1,20 @@
 import {ReactNode} from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header({children}: {children: ReactNode}) {
 	return (
 		<>
 			<div className={"fixed transition-opacity z-40 w-full h-16 border-b bg-transparent backdrop-blur"}>
-				<div className={"container items-center justify-between"}>
-					<div className={"rounded-full"}>
-						<Image src={"/profile.png"} alt={""} layout={"fill"}/>
-					</div>
+				<div className={"relative flex items-center h-full px-5 m-auto sm:container"}>
+					<Link href={"/"} >
+						<div className={"absolute flex flex-row items-center"}>
+							<div className={"relative w-12 h-12 mr-3"}>
+								<Image src={"/profile.png"} alt={""} layout={"fill"}/>
+							</div>
+							<h3 className={"text-xl font-bold"}>Frederik</h3>
+						</div>
+					</Link>
 
 				</div>
 			</div>
