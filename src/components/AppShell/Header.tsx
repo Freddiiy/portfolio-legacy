66414512c722e1199a6eb4c3408489d5839f5657
Freddiiy <frederikgaller@live.dk>
2022-06-30 17:42:12 +0200

    Some stuff tried with gradient background blobs, didnt work

--- src/components/AppShell/Header.tsx
+++ src/components/AppShell/Header.tsx
@@ -1,26 +1,121 @@
-import {ReactNode} from "react";
+import {Fragment, ReactNode, useRef, useState} from "react";
 import Image from "next/image";
 import Link from "next/link";
+import {GoMarkGithub} from "react-icons/go";
+import {FiMenu, FiX} from "react-icons/fi";
 
-export default function Header({children}: {children: ReactNode}) {
+import {Dialog, Transition} from "@headlessui/react";
+
+const links = [
+	{
+		name: "Home",
+		href: "/"
+	},
+	{
+		name: "Om mig",
+		href: "/about"
+	},
+	{
+		name: "Kontakt",
+		href: "/contact"
+	},
+]
+
+export default function Header({children}: { children: ReactNode }) {
+	const [isOpen, setIsOpen] = useState(false);
+	const open = () => setIsOpen(true);
+	const close = () => setIsOpen(false);
+
 	return (
 		<>
-			<div className={"fixed transition-opacity z-40 w-full h-16 border-b bg-transparent backdrop-blur"}>
-				<div className={"relative flex items-center h-full px-5 m-auto sm:container"}>
-					<Link href={"/"} >
-						<div className={"absolute flex flex-row items-center"}>
+			<div className={"fixed transition-opacity z-40 w-full h-16 bg-transparent backdrop-blur "}>
+				<div className={"relative flex items-center justify-between h-full px-5 m-auto sm:container"}>
+					<Link href={"/"} passHref>
+						<a className={"absolute flex flex-row items-center"}>
 							<div className={"relative w-12 h-12 mr-3"}>
-								<Image src={"/profile.png"} alt={""} layout={"fill"}/>
+								<Image src={"/profile.png"} alt={""} layout={"fill"} className={"rounded-full"}/>
 							</div>
 							<h3 className={"text-xl font-bold"}>Frederik</h3>
-						</div>
+						</a>
 					</Link>
 
+					<div className={"hidden m-auto md:flex justify-end"}>
+						{links.map((link) => (
+							<Link key={link.name} href={link.href}>
+								<a className={"mr-3 text-gray-300 hover:text-white cursor-pointer"}>
+									{link.name}
+								</a>
+							</Link>
+						))}
+					</div>
+
+					<div className={"flex"}>
+						<Link href={"https://github.com/Freddiiy"}>
+							<a>
+								<GoMarkGithub className={"text-white w-8 h-8"}/>
+							</a>
+						</Link>
+					</div>
+					<div className={"block md:hidden cursor-pointer"} onClick={open}>
+						<FiMenu className={"text-white w-8 h-8"}/>
+					</div>
 				</div>
 			</div>
-			<main className={"pt-16"}>
+			<main>
 				{children}
 			</main>
+
+			<ModalMenu/>
 		</>
 	)
+
+	function ModalMenu() {
+
+		let focusRef = useRef(null);
+
+		return (
+			<Transition appear show={isOpen} as={Fragment}>
+				<Dialog
+					as={"div"}
+					open={isOpen}
+					onClose={close}
+					className={"fixed inset-0 z-40 bg-black h-screen w-full"}
+				>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0 scale-95"
+						enterTo="opacity-100 scale-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100 scale-100"
+						leaveTo="opacity-0 scale-95"
+					>
+
+						<Dialog.Panel className={"w-full"}>
+							<div className={"flex justify-end mt-4 mr-5 cursor-pointer"} onClick={close}>
+								<FiX className={"w-8 h-8 text-white"}/>
+							</div>
+							<Dialog.Title
+								className={"text-lg text-2xl text-center font-medium text-white"}
+							>
+								Menu
+							</Dialog.Title>
+							<div className={"flex flex-col text-center mt-20"}>
+								{links.map((link) => (
+									<div key={link.name} className={"mb-5 text-4xl text-gray-300 hover:text-white cursor-pointer"}>
+										<Link key={link.name} href={link.href}>
+											<a>
+												{link.name}
+											</a>
+										</Link>
+									</div>
+								))}
+							</div>
+						</Dialog.Panel>
+					</Transition.Child>
+				</Dialog>
+			</Transition>
+		)
+	}
 }
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,45 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-	  <div className={"text-2xl text-blue-600"}>
-		  Hello World
-	  </div>
-  )
+
+	return (
+		<Landing/>
+	)
+
+	function Landing() {
+		return (
+			<>
+				<div className={"container mx-auto px-4 w-full pt-32 items-center justify-center"}>
+					<div className={"w-screen"}>
+					</div>
+					<div className={"grid grid-cols-1 sm:grid-cols-2"}>
+						<div className={"flex flex-col items-center justify-center"}>
+							<h1 className={"px-2 mb-3 text-4xl font-bold leading-tight text-center sm:text-6xl"}>
+								Hej, mit navn er {" "}
+								<br/>
+								<div
+									className={"inline text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600"}>
+									Frederik Galler
+								</div>
+							</h1>
+							<p className={"text-center max-w-2xl mt-1 mb-8 text-xl lg:text-2xl leading-tight lg:leading-8 text-gray-400"}>
+								og jeg er softwareudvikler med stor passion.
+							</p>
+						</div>
+						<div className={"flex items-center justify-center"}>
+							<div className={"relative w-96 h-96"}>
+								<Image src={"/me-with-cat.png"} alt={"Me with my cute cat Poul"} layout={"fill"}
+									   className={"rounded-2xl"}/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</>
+		)
+	}
 }
 
 export default Home
