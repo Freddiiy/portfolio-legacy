e2dfa6200b4e316734cc7e6ef00d66f55e8940aa
Freddiiy <frederikgaller@live.dk>
2022-07-02 11:03:11 +0200

    webudvikler

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,10 +35,10 @@ function Hero() {
 							Og jeg er full-stack webudvikler.
 						</p>
 						<div className={"relative w-full h-full -z-10"}>
-							<div className={"absolute -top-72 -left-4 w-96 h-96 bg-purple-500 rounded-full filter blur-3xl mix-blend-multiply opacity-70 animate-blob"}/>
-							<div className={"absolute -top-60 -right-0 w-96 h-96 bg-blue-500 rounded-full filter blur-3xl mix-blend-multiply opacity-70 animate-blob animate-delay-2s"}/>
-							<div className={"absolute -top-80 -right-20 w-96 h-96 bg-pink-500 rounded-full filter blur-3xl mix-blend-multiply opacity-70 animate-blob animate-delay-4s"}/>
-							<div className={"absolute -top-96 -left-8 w-96 h-96 bg-emerald-600 rounded-full filter blur-3xl mix-blend-multiply opacity-70 animate-blob animate-delay-6s"}/>
+							<div className={"absolute -top-60 -right-0 w-96 h-96 bg-blue-500 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob2 animate-delay-2s"}/>
+							<div className={"absolute -top-80 -right-20 w-96 h-96 bg-pink-500 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob animate-delay-4s"}/>
+							<div className={"absolute -top-72 -left-4 w-96 h-96 bg-purple-500 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob2"}/>
+							<div className={"absolute -top-96 -left-8 w-96 h-96 bg-emerald-600 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob animate-delay-6s"}/>
 						</div>
 					</div>
 					<div className={"absolute flex flex-col bottom-10 z-10"}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
                 'blob-two': "url('/blob-two.png')",
             },
             animation:  {
-                blob: "blob 14s ease infinite"
+                blob: "blob 8s ease infinite",
+                blob2: "blob2 6s ease infinite"
             },
             keyframes: {
                 blob: {
@@ -27,6 +28,20 @@ module.exports = {
                         transform: "scale(1) translate(0px, 0px)",
                     },
 
+                },
+                blob2: {
+                    "0%": {
+                        transform: "scale(1) translate(0px, 0px)",
+                    },
+                    "33%": {
+                        transform: "scale(1.3) translate(-50px, 70px)",
+                    },
+                    "66%": {
+                        transform: "scale(0.8) translate(30px, -40px)",
+                    },
+                    "100%": {
+                        transform: "scale(1) translate(0px, 0px)",
+                    },
                 }
             }
         },
