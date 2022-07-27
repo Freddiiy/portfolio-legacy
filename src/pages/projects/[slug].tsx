72b74da8f1b26d1ce64ed57424110ec4cd562f29
Freddiiy <frederikgaller@live.dk>
2022-07-27 18:33:17 +0200

    project fix type and removed .at at the project array, it didnt wanna compile

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,79 +1,80 @@
 import {
-  GetStaticPaths,
-  GetStaticProps,
-  InferGetServerSidePropsType,
-  InferGetStaticPropsType,
-  NextPage,
+	GetStaticPaths,
+	GetStaticProps,
+	InferGetServerSidePropsType,
+	InferGetStaticPropsType,
+	NextPage,
 } from "next";
 import Head from "next/head";
 import slugify from "slugify";
-import { IProject, projects } from ".";
-import { ParsedUrlQuery } from "querystring";
+import {IProject, projects} from ".";
+import {ParsedUrlQuery} from "querystring";
 import Image from "next/image";
-import { FaGithub } from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
 import Link from "next/link";
 
 interface IParams extends ParsedUrlQuery {
-  slug: string;
+	slug: string;
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = projects.map((p) => ({
-    params: { slug: slugify(p.title.toLowerCase()) },
-  }));
-  return { paths, fallback: false };
+	const paths = projects.map((p) => ({
+		params: {slug: slugify(p.title.toLowerCase())},
+	}));
+	return {paths, fallback: false};
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { slug } = ctx.params as IParams;
-  const project = projects
-    .filter((p) => slugify(p.title.toLowerCase()) === slug)
-    .at(0);
-  return { props: { project } };
+	const {slug} = ctx.params as IParams;
+	const projectArray = projects.filter(
+		(p) => slugify(p.title.toLowerCase()) === slug
+	);
+	const project = projectArray[0];
+	return {props: {project}};
 };
 
-function Project({ project }: { project: IProject }) {
-  return (
-    <>
-      <Head>
-        <title>{project.title}</title>
-      </Head>
-      <section className="container mx-auto pt-28 px-4">
-        <div className="block mb-6">
-          <h2 className="text-4xl md:text-6xl font-bold md:pb-1">
-            {project.title}
-          </h2>
-          <div className="flex flex-row ml-3 items-center justify-start mb-4">
-            {project.github && (
-              <Link href={project.github} passHref>
-                <a className="mr-3 text-gray-300 hover:text-white cursor-pointer">
-                  <FaGithub />
-                </a>
-              </Link>
-            )}
-            {project.live && (
-              <Link href={project.live}>
-                <a className="mr-3 text-gray-300 hover:text-white cursor-pointer">
-                  Live
-                </a>
-              </Link>
-            )}
-          </div>
-          {project.img && (
-            <div className="relative h-72 w-72">
-              <Image
-                src={project.img}
-                alt=""
-                layout="fill"
-                className="rounded-xl"
-              />
-            </div>
-          )}
-        </div>
-        <p>{project.desc}</p>
-      </section>
-    </>
-  );
+function Project({project}: {project: IProject}) {
+	return (
+		<>
+			<Head>
+				<title>{project.title}</title>
+			</Head>
+			<section className="container mx-auto pt-28 px-4">
+				<div className="block mb-6">
+					<h2 className="text-4xl md:text-6xl font-bold md:pb-1">
+						{project.title}
+					</h2>
+					<div className="flex flex-row ml-3 items-center justify-start mb-4">
+						{project.github && (
+							<Link href={project.github} passHref>
+								<a className="mr-3 text-gray-300 hover:text-white cursor-pointer">
+									<FaGithub />
+								</a>
+							</Link>
+						)}
+						{project.live && (
+							<Link href={project.live}>
+								<a className="mr-3 text-gray-300 hover:text-white cursor-pointer">
+									Live
+								</a>
+							</Link>
+						)}
+					</div>
+					{project.img && (
+						<div className="relative h-72 w-72">
+							<Image
+								src={project.img}
+								alt=""
+								layout="fill"
+								className="rounded-xl"
+							/>
+						</div>
+					)}
+				</div>
+				<p>{project.desc}</p>
+			</section>
+		</>
+	);
 }
 
 export default Project;
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,103 +1,99 @@
-import { GetStaticPaths, NextPage } from "next";
+import {GetStaticPaths, NextPage} from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import slugify from "slugify";
-import { UrlObject } from "url";
+import {UrlObject} from "url";
 
 export interface IProject {
-  title: string;
-  desc: string;
-  img?: string;
-  github?: string;
-  live?: string;
+	title: string;
+	desc: string;
+	img: string;
+	github?: string;
+	live?: string;
 }
 
 export let projects: IProject[] = [
-  {
-    title: "WOW Mount Viewer",
-    desc: "Skoleproject. En webapp til at tracke hvilke mounts man ejer i wow.",
-    img: "/projects/mount-viewer.png",
-    github: "https://github.com/Freddiiy/mount-viewer-frontend",
-    live: "https://mount-viewer.vercel.app/",
-  },
-  {
-    title: "CG Care hjemmeside",
-    desc: "Simpel hjemmeside til CG Care massagefirma med prisudregner.",
-    img: "/projects/cgcare.png",
-    live: "https://cgcare.dk/",
-  },
-  {
-    title: "FOG Carport Builder",
-    desc: "Eksamensprojekt hos CPHBusiness. Gruppeopgave.",
-    img: "/projects/fog-project.png",
-    github: "https://github.com/Freddiiy/cphbusiness-fog",
-    live: "https://www.youtube.com/watch?v=7VPCYVwcpFs",
-  },
+	{
+		title: "WOW Mount Viewer",
+		desc: "Skoleproject. En webapp til at tracke hvilke mounts man ejer i wow.",
+		img: "/projects/mount-viewer.png",
+		github: "https://github.com/Freddiiy/mount-viewer-frontend",
+		live: "https://mount-viewer.vercel.app/",
+	},
+	{
+		title: "CG Care hjemmeside",
+		desc: "Simpel hjemmeside til CG Care massagefirma med prisudregner.",
+		img: "/projects/cgcare.png",
+		live: "https://cgcare.dk/",
+	},
+	{
+		title: "FOG Carport Builder",
+		desc: "Eksamensprojekt hos CPHBusiness. Gruppeopgave.",
+		img: "/projects/fog-project.png",
+		github: "https://github.com/Freddiiy/cphbusiness-fog",
+		live: "https://www.youtube.com/watch?v=7VPCYVwcpFs",
+	},
 ];
 
 const Projects: NextPage = () => {
-  let testProject = projects
-    .filter((p) => slugify(p.title.toLowerCase()) === "wow-mount-viewer")
-    .at(0);
-  return (
-    <>
-      <Head>
-        <title>Projects</title>
-      </Head>
-      <section className="container mx-auto pt-28 pb-10">
-        <div className="block mb-6 ml-3">
-          <h2 className="text-4xl md:text-6xl font-bold md:pb-3">
-            Mine projekter
-          </h2>
-          <p>{testProject?.title}</p>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 px-3">
-          {projects.map((project, i) => (
-            <ProjectCard
-              key={i}
-              title={project.title}
-              desc={project.desc}
-              img={project.img}
-              href={slugify(project.title.toLowerCase())}
-            />
-          ))}
-        </div>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Projects</title>
+			</Head>
+			<section className="container mx-auto pt-28 pb-10">
+				<div className="block mb-6 ml-3">
+					<h2 className="text-4xl md:text-6xl font-bold md:pb-3">
+						Mine projekter
+					</h2>
+				</div>
+				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 px-3">
+					{projects.map((project, i) => (
+						<ProjectCard
+							key={i}
+							title={project.title}
+							desc={project.desc}
+							img={project.img}
+							href={slugify(project.title.toLowerCase())}
+						/>
+					))}
+				</div>
+			</section>
+		</>
+	);
 };
 
 function ProjectCard({
-  title,
-  desc,
-  img,
-  href,
+	title,
+	desc,
+	img,
+	href,
 }: {
-  title: string;
-  desc: string;
-  img: string;
-  href: string | UrlObject;
+	title: string;
+	desc: string;
+	img: string;
+	href: string | UrlObject;
 }) {
-  return (
-    <Link href={`/projects/${href}`} passHref>
-      <div className="group border bg-neutral-900 border-gray-300 border-opacity-50 hover:border-gray-300 hover:border-opacity-100 transition-all duration-200 rounded-lg hover:cursor-pointer">
-        <div className="relative w-full h-64 overflow-hidden border-black transition-all duration-300 rounded-t-lg bg-">
-          <Image
-            src={img}
-            alt={""}
-            layout="fill"
-            className="group-hover:scale-125 transition-all duration-500 rounded-t-lg"
-          />
-        </div>
-        <div className="p-5">
-          <h4 className="pb-2 font-semibold text-lg">{title}</h4>
-          <p className="text-sm text-gray-300">{desc}</p>
-        </div>
-        <></>
-      </div>
-    </Link>
-  );
+	return (
+		<Link href={`/projects/${href}`} passHref>
+			<div className="group border bg-neutral-900 border-gray-300 border-opacity-50 hover:border-gray-300 hover:border-opacity-100 transition-all duration-200 rounded-lg hover:cursor-pointer">
+				<div className="relative w-full h-64 overflow-hidden border-black transition-all duration-300 rounded-t-lg bg-">
+					<Image
+						src={img}
+						alt={""}
+						layout="fill"
+						className="group-hover:scale-125 transition-all duration-500 rounded-t-lg"
+					/>
+				</div>
+				<div className="p-5">
+					<h4 className="pb-2 font-semibold text-lg">{title}</h4>
+					<p className="text-sm text-gray-300">{desc}</p>
+				</div>
+				<></>
+			</div>
+		</Link>
+	);
 }
 
 export default Projects;
