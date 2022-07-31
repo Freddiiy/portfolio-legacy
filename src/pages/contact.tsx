5509c75b5067a709385b8d01116aecec40311b08
Freddiiy <frederikgaller@live.dk>
2022-07-31 15:24:38 +0200

    asd

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,13 +4,15 @@ import ContactForm from "../features/Contact/ContactForm";
 import ContactSection from "../features/Contact/ContactPage";
 import {useLocale} from "../hooks/useLocale";
 import headerLocale from "../locales/header.json";
+import {useRouter} from "next/router";
 
 const Contact: NextPage = () => {
 	let headerText = useLocale(headerLocale);
+	const router = useRouter()
 	return (
 		<>
 			<Head>
-				<title>{headerText.links[2].name}</title>
+				<title>{router.locale == "da-DK" ? "Kontakt" : "Contact"}</title>
 			</Head>
 			<section className="container mx-auto pt-24">
 				<ContactSection />
