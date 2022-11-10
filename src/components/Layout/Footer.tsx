26242c684e0104f8154bd9847322aaaf5fd71ece
Freddiiy <frederikgaller@live.dk>
2022-11-10 15:49:55 +0100

    asd

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -11,10 +11,10 @@ export default function Footer() {
       <div className="flex text-center items-center justify-center text-lg">
         Made with <FaHeart className="inline mx-1 text-red-600" /> from{" "}
         <span className={"relative inline flex items-center group ml-1 w-12"}>
-			<span className={"w-12 absolute inline opacity-100 group-hover:opacity-0 translate-y-0 group-hover:-translate-y-4 transition-all duration-100"}>
+			<span className={"w-12 absolute opacity-100 group-hover:opacity-0 translate-y-0 group-hover:-translate-y-4 transition-all duration-100"}>
 				Copenhagen
 			</span>
-			<span className={"w-12 absolute inline opacity-0 group-hover:opacity-100 translate-y-4 group-hover:translate-y-0 transition-all duration-100"}>
+			<span className={"w-12 absolute opacity-0 group-hover:opacity-100 translate-y-4 group-hover:translate-y-0 transition-all duration-100"}>
 				Warde
 			</span>
 		</span>
